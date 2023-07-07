572ca76569478b8e6a0e433874da72b433d2e6f9
Vinay <Vinay@example.com>
2023-07-07 17:47:43 +0530

    debugging for deployment

--- xIRA/settings.py
+++ xIRA/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5&_+!m=5ek9coss18ixy2ux9d&g%v53gi(as_x#*271j-qsi6p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
